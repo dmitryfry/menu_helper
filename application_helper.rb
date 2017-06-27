a0a186f702f3648ca5ad1401d0ccb8115fa7fd77
Dmitry Fry <im@dmitryfry.com>
2017-06-27 12:37:07 +0300

    Initial commit

---
+++ application_helper.rb
@@ -0,0 +1,21 @@
+module ApplicationHelper
+
+  def menu
+    [
+      menu_item('main', root_path),
+      menu_item('about', about_path),
+      menu_item('gallery', gallery_path),
+      menu_item('prices', prices_path),
+      menu_item('articles', articles_1_path),
+      menu_item('contacts', contacts_path),
+    ].join.html_safe
+  end
+
+  def menu_item(text, path)
+    options = current_page?(path) ? { class: "active" } : {}
+      content_tag(:li, options) do
+        link_to text, path
+      end
+  end
+
+end
